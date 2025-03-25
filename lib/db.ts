3d00561f9415d151d2270134b3faf31a652d8710
Gowtham T <gowthamtj0808@gmail.com>
2025-03-25 12:41:50 +0530

    changes

--- lib/db.ts
+++ lib/db.ts
@@ -1,19 +1,11 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client"
 
-// Extend the global object for TypeScript
-declare global {
-  namespace NodeJS {
-    interface Global {
-      prisma?: PrismaClient;
-    }
-  }
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined
 }
 
-// Check if Prisma instance already exists to avoid multiple instances in development
-const globalForPrisma = global as unknown as { prisma?: PrismaClient };
-
-export const db = globalForPrisma.prisma ?? new PrismaClient();
+export const db = globalForPrisma.prisma ?? new PrismaClient()
 
 if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = db;
+  globalForPrisma.prisma = db
 }
