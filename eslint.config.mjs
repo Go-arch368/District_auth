e737649bea7cc38284e6b916e24c3cfbdcaf9780
Gowtham T <gowthamtj0808@gmail.com>
2025-03-26 14:23:05 +0530

    husky

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import { error } from "console";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,8 +10,17 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
-
+const eslintConfig = {
+  extends: [
+    ...compat.extends("next/core-web-vitals", "next/typescript"),
+    "eslint:recommended",
+    "next",
+    "next/core-web-vitals",  // Note: This is duplicated from the compat.extends
+    "prettier"
+  ],
+  "plugins":["prettier"],
+  "rules":{
+    "prettier/prettier":"error"
+  }
+};
 export default eslintConfig;
