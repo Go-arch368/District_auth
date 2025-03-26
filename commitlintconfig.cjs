4c3c12b7a700a55d35ac569269c18f2ad0fd59f2
Gowtham T <gowthamtj0808@gmail.com>
2025-03-26 22:23:40 +0530

    fix: correct example message

---
+++ commitlintconfig.cjs
@@ -0,0 +1,26 @@
+module.exports ={
+    extends: ['@commitlint/config-conventional'],
+    rules:{
+        "header-min-length": [2, "always", 10],
+        "header-max-length": [2, "always", 100],
+        "header-case": [2, "always", "lower-case"],
+    },
+    plugins:[
+        {
+            rules:{
+              "header-case-start-capital":({raw})=>{
+                return[
+                    /^# [A-Z]/.test(raw[0]),
+                    "Commit message should start with a capital letter"
+                ]
+              }
+            },
+            "header-end-period":({raw})=>{
+                return[
+                    /\.$/.test(raw[0]),
+                    "Commit message should end with a period"
+                ]
+            }
+        }
+    ]
+}
